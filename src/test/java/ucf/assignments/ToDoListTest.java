102c37906733537442a81db724b759a1424afac9
Rey <reyfundora@icloud.com>
2021-07-11 23:22:03 -0400

    Commit

--- src/test/java/ucf/assignments/ToDoListTest.java
+++ src/test/java/ucf/assignments/ToDoListTest.java
@@ -7,26 +7,20 @@ package ucf.assignments;
 
 import javafx.scene.Parent;
 import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class ToDoListTest {
     @Test
-    void is_button_that_views_all_working() {
-        // Assign value to static variable if method runs.
-        // Expected: Hard-coded value; actual: value after it runs
-    }
-    @Test
-    void is_button_that_views_all_working2() {
-        // Assign value to static variable if method runs.
-        // Expected: Hard-coded value; actual: value after it runs
-    }
-    @Test
-    void is_button_that_views_all_working3() {
-        // Assign value to static variable if method runs.
-        // Expected: Hard-coded value; actual: value after it runs
-    }
-    @Test
-    void is_button_that_views_all_working4() {
-        // Assign value to static variable if method runs.
-        // Expected: Hard-coded value; actual: value after it runs
+    void isItemCounterVariableEqualtoNegative() {
+        // given
+        int expect = -1;
+
+        // when
+        int actual = Edit.itemCounter;
+
+        // then
+        assertEquals(expect, actual);
     }
 }
